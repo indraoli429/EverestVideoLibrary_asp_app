13a4b857dfec1575a7632ab91553d5078fa97445
Indra Bahadur Oli <indraoli429@gmail.com>
2020-04-30 03:34:55 +0545

    Adding material design, add bootstrap 4.4 then use of metadata and partial class

---
+++ EverestVideoLibraryApp/Models/Metadata.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EverestVideoLibraryApp.Models
+{
+    public class Metadata
+    {
+    }
+    public class ArtistMetadata
+    {
+        [Display(Name = "First Name")]
+        public string FirstName;
+
+        [Display(Name = "Last Name")]
+        public string LastName;
+    }
+    public class DVDCopyMetadata
+    {
+        [Display(Name = "DVDCopyId")]
+        public int DCopyId;
+    }
+
+    public class LoanMetadata
+    {
+        [Display(Name = "Due Amount")]
+        public string DueAmount;
+        [Display(Name = "Fine Amount")]
+        public string FineAmount;
+    }
+
+    public class MemberMetadata
+    {
+        [Display(Name = "First Name")]
+        public string FirstName;
+        [Display(Name = "Last Name")]
+        public string LastName;
+    }
+
+    public class MemberCategoryMetadata
+    {
+        [Display(Name = "Total Days")]
+        public int TotalDays;
+        [Display(Name = "Total Loan")]
+        public int TotalLoan;
+        [Display(Name = "Fine Amount Per day")]
+        public int FineAmountPerDay;
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "Full Name")]
+        public string FullName;
+    }
+}
---
+++ EverestVideoLibraryApp/Models/PartialClasses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EverestVideoLibraryApp.Models
+{
+    public class PartialClasses
+    {
+    }
+
+    [MetadataType(typeof(ArtistMetadata))]
+    public partial class Artist { }
+
+    [MetadataType(typeof(DVDCopyMetadata))]
+    public partial class DVDCopy { }
+
+    [MetadataType(typeof(LoanMetadata))]
+    public partial class Loan { }
+
+    [MetadataType(typeof(MemberMetadata))]
+    public partial class Member { }
+
+    [MetadataType(typeof(MemberCategoryMetadata))]
+    public partial class MemberCategory { }
+
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User { }
+}
